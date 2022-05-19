8376eec03a091fd270de5f13215fcf9368c45f15
KrystinaGerke <pictureofarose@gmail.com>
2022-05-19 10:39:46 -0400

    Added MobX

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,7 +1,4 @@
 import axios, { AxiosResponse } from 'axios';
-import { resolve } from 'dns';
-import { request } from 'http';
-import { UrlWithStringQuery } from 'url';
 import { Activity } from '../models/activity';
 
 const sleep = (delay: number) => {
